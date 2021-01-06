5e852c31361abe681e4bc4b58b3edf11d4acde61
Void <void@algonell.com>
2021-01-06 23:51:52 +0200

    added factory method

---
+++ Design Patterns/org/algonell/trading/dp/factorymethod/FactoryMethodDemo.java
@@ -0,0 +1,23 @@
+package org.algonell.trading.dp.factorymethod;
+
+/**
+ * Factory Method demo: use real-time data provider across all code base, change easily if needed.
+ * 
+ * @author Andrew Kreimer
+ *
+ */
+public class FactoryMethodDemo {
+
+	private static final org.apache.logging.log4j.Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger(FactoryMethodDemo.class);
+	
+	public static void main(String[] args) {
+		//using IB
+		DataProvider ibProvider = DataProviderFactory.createProvider(DataProviderType.IB);
+		LOGGER.info(ibProvider.getData());
+		
+		//using Quandl
+		DataProvider quandlProvider = DataProviderFactory.createProvider(DataProviderType.QUANDL);
+		LOGGER.info(quandlProvider.getData());
+	}
+
+}
---
+++ Design Patterns/org/algonell/trading/dp/factorymethod/IbDataProvider.java
@@ -0,0 +1,21 @@
+package org.algonell.trading.dp.factorymethod;
+
+/**
+ * Interactive Brokers data provider.
+ * Uses TWS API to get historical data (bar, unit & period).
+ * 
+ * @author Andrew Kreimer
+ *
+ */
+public class IbDataProvider implements DataProvider {
+
+	@Override
+	public String getData() {
+		//IB data requests
+		
+		//publish-subscribe
+		
+		return "Data from IB";
+	}
+
+}
