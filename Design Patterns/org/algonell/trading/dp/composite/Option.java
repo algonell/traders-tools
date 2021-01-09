9bdff6fbd1eb12131e1a9517cb08b47e31fc7b21
Void <void@algonell.com>
2021-01-09 13:46:26 +0200

    added composite

---
+++ Design Patterns/org/algonell/trading/dp/composite/Option.java
@@ -0,0 +1,13 @@
+package org.algonell.trading.dp.composite;
+
+/**
+ * Option is the building block of any options position.
+ *  
+ * @author Andrew Kreimer
+ *
+ */
+public interface Option {
+
+	public double calculateDelta();
+	
+}
