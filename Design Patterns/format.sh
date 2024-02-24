1d6b554be843e5d669af5cb9c619879091a78c90
void <algonell@gmail.com>
2024-02-24 11:38:58 +0200

    style: format

--- Design Patterns/format.sh
+++ Design Patterns/format.sh
@@ -2,10 +2,10 @@
 
 clear
 
+echo
 echo "google-java-format..."
 java -jar ~/Misc/google-java-format-*.jar --replace $(git ls-files *.java)
 
 echo
-
 echo "SpotBugs..."
 mvn -f ~/git/traders-tools/ spotbugs:check
