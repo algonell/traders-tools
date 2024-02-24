a60ecff29974cb4b24c8491a0d1aef9e2691e7b2
void <algonell@gmail.com>
2024-02-24 12:12:23 +0200

    feat: format XML on update

--- Design Patterns/update.sh
+++ Design Patterns/update.sh
@@ -2,6 +2,7 @@
 
 echo "Maven..."
 mvn -f ~/git/traders-tools versions:use-latest-releases -DgenerateBackupPoms=false
+find ~/git/traders-tools -name "*.xml" -exec xmllint --format '{}' --output '{}' \;
 
 echo "build..."
 ~/git/traders-tools/Design\ Patterns/build.sh
