c6cf42094a15affc71106c940a47db2e17dc432a
Andrew Kreimer <algonell@gmail.com>
2024-07-08 10:51:13 +0300

    feat: improve format script

--- Design Patterns/format.sh
+++ Design Patterns/format.sh
@@ -2,10 +2,17 @@
 
 clear
 
+# codespell
+echo
+echo "codespell..."
+~/.venv/bin/codespell -L ErrorProne,errorprone $(git ls-files ~/git/traders-tools/)
+
+# google-java-format
 echo
 echo "google-java-format..."
-java -jar ~/Misc/google-java-format-*.jar --replace $(git ls-files *.java)
+~/.sdkman/candidates/java/current/bin/java -jar ~/Misc/google-java-format-*.jar --replace $(git ls-files ~/git/traders-tools/*.java)
 
+# SpotBugs
 echo
 echo "SpotBugs..."
 mvn -f ~/git/traders-tools/ spotbugs:check
